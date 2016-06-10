79f7da8a0f1848d6ea053239412cc785f30ef258
berziiii <brian@offpistedesigns.com>
2016-06-10 13:45:58 -0400

    Add code for session
    Add Album code to show class methods vs instance methods

---
+++ lib/album.rb
@@ -0,0 +1,38 @@
+#Album class
+class Album
+  attr_reader :artist, :title
+  attr_accessor :tracks
+
+  @@collection = []
+
+  def initialize(artist:, title:)
+    @artist = artist
+    @title = title
+    @tracks = []
+
+    @@collection << self
+  end
+
+  def self.all
+    @@collection
+  end
+
+  def self.find_by_title(query)
+    @@collection.select { |album| album.title == query }
+  end
+end
+
+class Song
+  attr_reader :title
+
+  def initialize(title:)
+    @title = title
+  end
+end
+
+# t_swift = Album.new(artist: 'Taylor Swift', title: '1989')
+# t_swift.tracks << Song.new(title: 'Shake It Off')
+# t_swift.tracks << Song.new(title: 'Bad Blood')
+#
+# nevermind = Album.new(artist: 'Nirvana', title: 'Nevermind')
+# nevermind.tracks << Song.new(title: 'Smells Like Teen Spirit')
